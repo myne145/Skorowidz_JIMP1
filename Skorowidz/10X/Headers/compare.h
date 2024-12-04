c37aaa76a9ddff7cfffb42745249d158c923b8ed
myne145 <antoni.kucz@gmail.com>
2024-12-04 22:11:07 +0100

    poprawione pliki (może nawet nie usunięte z repo)

---
+++ Skorowidz/10X/Headers/compare.h
@@ -0,0 +1,16 @@
+//
+//  compare.h
+//  Skorowidz1
+//
+//  Created by Oskar Przybylski on 22/11/2024.
+//
+#ifndef COMPARE_H
+#define COMPARE_H
+#include "kw_t.h"
+
+// Deklaracje funkcji
+int compare(const void* kw1,const void* kw2);
+
+#endif // COMPARE_H
+
+
---
+++ Skorowidz/10X/Unit Tests/fifo_t.c
@@ -0,0 +1,30 @@
+//
+//  fifo_t.c
+//  Skorowidz1
+//
+//  Created by Oskar Przybylski on 26/11/2024.
+//
+
+
+#include "../Headers/fifo_t.h"
+#include "../Headers/kw_t.h"
+#include <stdio.h>
+
+int main(void){
+    
+    kw_t* kw1 = keyWordInit("Keyword");
+    kw_t* kw2 = keyWordInit("KW2");
+    
+    fifo_t* que1 = init(kw1);
+    push(que1, kw2);
+    
+    print_fifo(que1);
+    
+    kw_t* kw3 = pop(&que1);
+    
+    print_fifo(que1);
+    
+    push(que1, kw3);
+    
+    print_fifo(que1);
+}
